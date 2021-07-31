cfa8a13be420a35cab671b435c330b59c1d885b7
Ustim <ustimenkodenis@mail.ru>
2021-07-31 17:16:12 +0300

    feat: first commit on  GitActions project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {interval} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'MyRxJs';
+  phones = ["iPhone 7", "LG G 5", "Honor 9", "Idol S4", "Nexus 6P"];
+  phone$ = interval(1000).pipe(map( (i: number )=> this.phones[i]));
 }
